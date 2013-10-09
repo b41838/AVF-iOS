fb9680367d6d02bbd656999bcdadf4894d1ac563
Kyler Schroeder <kylerschroeder@fullsail.edu>
2013-10-08 22:22:51 -0600

    Working on Instagram
    Added a textbox to add a hashtag and button to search.

--- www/js/main.js
+++ www/js/main.js
@@ -1,2 +1,33 @@
 // JavaScript Document
 
+$('#instagramFeed').on('pageinit', function() {
+
+	// Instagram API
+	
+	// get variable from textbox
+		$("#instaButton").click(function() {
+			var tag = $("#hashtag").val();
+			console.log(tag);
+		});
+	
+	var url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=9a4423b4dfdd4111a73d4bd39082f519&amp;count=12";
+	
+	$.getJSON(url, getFeed);
+});
+
+//function getFeed(info) {
+var getFeed = function(info) {
+
+	console.log(info);
+	
+	$("#instagramStatus").html("<h2>Instagram Results:</h2>");
+	
+	$.each(info.data, function(index, photo) {  // index is position in array of info.data
+		var pic = "<li><img src='" + photo.images.thumbnail.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + 			", <em>(" + photo.user.name +")</em></h4></li>";
+		$("#pullFeed").append(pic);
+	}); // end each
+	
+	$("li:nth-child(3n+1)").addClass("ui-block-a");
+	$("li:nth-child(3n+2)").addClass("ui-block-b");
+	$("li:nth-child(3n+3)").addClass("ui-block-c");
+}; // end screenOutput
